0a2324d6f1f49211540b1ee8fb88dd36a163d762
smikos22 <mik22003@byui.edu>
2023-02-20 13:48:32 -0700

    Max number in list

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,39 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+        string userInput;
+        int listOfNumbers;
+        int maxVal = 0;
+        Console.WriteLine("Enter a list of numbers, type 0 when finished");
+
+        do
+        {
+            Console.Write("Enter number: ");
+            userInput = Console.ReadLine();
+            listOfNumbers = int.Parse(userInput);
+            if (listOfNumbers != 0)
+            {
+                numbers.Add(listOfNumbers);
+            }
+        } while (listOfNumbers != 0);
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum += number;
+        
+ //          Finding the Max value in a list of numbers
+            foreach( int i in numbers)
+            {
+                if (i > maxVal) {
+                    maxVal = i;
+                }
+            
+            }
+        }
+        Console.WriteLine($"The sum is: {sum}");
+
+        Console.WriteLine($"The average is: {(float) sum/numbers.Count}");
+        Console.WriteLine($"The largest number is: {maxVal}");
     }
 }
