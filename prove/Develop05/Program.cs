7b22bcb966694034bcb20425e6e1751a7d226cf2
smikos22 <mik22003@byui.edu>
2023-03-24 17:53:40 -0700

    start of polymorphism

--- prove/Develop05/Program.cs
+++ prove/Develop05/Program.cs
@@ -3,7 +3,49 @@ using System;
 class Program
 {
     static void Main(string[] args)
+    
     {
-        Console.WriteLine("Hello Develop05 World!");
-    }
+        int menuUserInput = 0;
+        string endMessage = "";
+
+        List<string> menu = new List<string>
+            {
+                "Menu Options:",
+                "1. Create New Goal",
+                "2. List Goals",
+                "3. Save Goals",
+                "4. Load Goals",
+                "5. Record Event",
+                "6. Quit",
+                "Select a choice from the menu: "
+            };
+        while (menuUserInput != 6)
+        {
+            foreach (string menuItem in menu)
+            {
+                Console.WriteLine(menuItem);
+            }
+
+            menuUserInput = int.Parse(Console.ReadLine());
+
+            if (menuUserInput == 1)
+            {
+                string bStartMessage = "The types of Goals are:";
+            }
+            else if (menuUserInput == 2)
+            {
+            }
+            else if (menuUserInput == 3)
+            {
+            }
+        }
+    
+
+
+        
+
+
+    }   
+    
+  
 }
