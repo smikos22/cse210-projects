9ff1fb4c7a9f94fea8889ffe02be68960de68499
smikos22 <mik22003@byui.edu>
2023-03-27 19:27:40 -0700

    Final Abstraction program

--- final/Foundation1/Program.cs
+++ final/Foundation1/Program.cs
@@ -4,20 +4,72 @@ class Program
 {
     static void Main(string[] args)
     {
-        Video video1 = new Video();
-        video1._name = "Sarah Mikos";
-        video1._comments="Loved the video";
-        video1._author
-        video1._comments= 2019;
-        video1._comments= 2022;
- //       job1.Display();
+        Comment v1 = new Comment();
+        v1._name = "Sarah Mikos";
+        v1._comment="Loved the video";
 
-        Job job2 = new Job();
-        job2._company = "Apple";
-        job2._jobTitle="Manager"; 
-        job2._startYear= 2022; 
-        job2._endYear=2023;
-//        job2.Display();
+        Comment v1c2 = new Comment();
+        v1c2._name = "Gage Mikos";
+        v1c2._comment ="Very educational";
+
+        Comment v1c3 = new Comment();
+        v1c3._name = "Shasta Vega";
+        v1c3._comment = "Amazing";
+
+        Video video1 = new Video();
+        video1._title = "How to learn to program with c#";
+        video1._author = "Shane Master";
+        video1._length = 1000;
+        video1._comments.Add(v1);
+        video1._comments.Add(v1c2);
+        video1._comments.Add(v1c3);
+        video1.DisplayVideo();
+        Console.WriteLine();
+
+        Comment v2 = new Comment();
+        v2._name = "Chris Lane";
+        v2._comment="Best video I've watched";
+
+        Comment v2c2 = new Comment();
+        v2c2._name = "Zach Murphy";
+        v2c2._comment ="Please make more videos";
+
+        Comment v2c3 = new Comment();
+        v2c3._name = "Herman Body";
+        v2c3._comment = "Thank you for your help";
+
+        Video video2 = new Video();
+        video2._title = "Python for Beginners";
+        video2._author = "Monty Python";
+        video2._length = 5000;
+        video2._comments.Add(v2);
+        video2._comments.Add(v2c2);
+        video2._comments.Add(v2c3);
+        video2.DisplayVideo();
+        Console.WriteLine();
+
+        Comment v3 = new Comment();
+        v3._name = "Sam Chewy";
+        v3._comment="Bravo";
+
+        Comment v3c2 = new Comment();
+        v3c2._name = "Brock Lee";
+        v3c2._comment ="Keep making more!";
+
+        Comment v3c3 = new Comment();
+        v1c3._name = "Mckaela Night";
+        v1c3._comment = "This is wonderful";
+
+        Video video3 = new Video();
+        video3._title = "HTML for beginners";
+        video3._author = "Mandy Green";
+        video3._length = 2000;
+        video3._comments.Add(v1);
+        video3._comments.Add(v1c2);
+        video3._comments.Add(v1c3);
+        video3.DisplayVideo();
+        Console.WriteLine();
+
 
     }
 }
--- final/Foundation1/Video.cs
+++ final/Foundation1/Video.cs
@@ -4,6 +4,20 @@ public class Video
     public string _author;
     public int _length;
     public List<Comment> _comments = new List<Comment> ();
-    
-    
+
+    public void DisplayVideo()
+    {
+        Console.WriteLine($"Title: {_title}");
+        Console.WriteLine($"Author: {_author}");
+        Console.WriteLine($"Length:{_length} secconds");
+        foreach(Comment comment in _comments)
+        {
+            Console.WriteLine($"Name: {comment._name} Comment: {comment._comment}");
+        }
+        Console.WriteLine($"Number of comments: {CountComments()}");
+    }
+    public int CountComments()
+    {
+        return _comments.Count;
+    }
 }
